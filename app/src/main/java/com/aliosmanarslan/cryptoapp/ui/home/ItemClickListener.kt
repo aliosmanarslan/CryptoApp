f69145306534cd1dac2b7e5c385f36e1b1b90ce3
Ali Osman ARSLAN <aliarslanic@gmail.com>
2022-05-03 16:57:55 +0300

    Card Design DataBinding

---
+++ app/src/main/java/com/aliosmanarslan/cryptoapp/ui/home/ItemClickListener.kt
@@ -0,0 +1,10 @@
+package com.aliosmanarslan.cryptoapp.ui.home
+
+import com.aliosmanarslan.cryptoapp.model.home.Data
+
+/**
+ * Created by Ali Osman ARSLAN on 3.05.2022
+ */
+interface ItemClickListener {
+    fun onItemClick(coin: Data)
+}
